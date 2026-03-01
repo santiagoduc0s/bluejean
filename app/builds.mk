6248922f7ed356e6844f5f9f1de63ebb4cebbc51
santiagoduc0s <santidu200@gmail.com>
2026-03-01 16:47:55 -0300

    save

--- app/builds.mk
+++ app/builds.mk
@@ -1,7 +1,7 @@
 build-ios-prod-ipa:
 	make prod
-	fvm flutter build ipa --obfuscate --split-debug-info --release --flavor production --target lib/main_production.dart --dart-define-from-file env.json
+	fvm flutter build ipa --obfuscate --split-debug-info --release --flavor production --target lib/main.dart --dart-define-from-file env.json
 
 build-android-prod:
 	make prod
-	fvm flutter build appbundle --flavor production --target lib/main_production.dart --dart-define-from-file env.json
+	fvm flutter build appbundle --flavor production --target lib/main.dart --dart-define-from-file env.json
--- app/lib/main_production.dart
+++
@@ -1,6 +0,0 @@
-import 'package:lune/bootstrap.dart';
-import 'package:lune/ui/main/main.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
